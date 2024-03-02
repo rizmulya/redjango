c2d9e2896752df3d612430bd43765fefb2585e08
rizmulya <riskymulya@protonmail.com>
2024-03-02 21:39:09 +0700

    chore(static): Enable static files on DEBUG=False

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 ```
 npm run build
+python manage.py collectstatic
 python manage.py runserver
 ```
--- redjango/settings.py
+++ redjango/settings.py
@@ -29,7 +29,7 @@ env = {
 SECRET_KEY = env['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = env['ALLOWED_HOST'].split('|')
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -125,6 +126,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR.joinpath('frontend', 'dist'),
 )
+
+STATIC_ROOT = BASE_DIR.joinpath('static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
